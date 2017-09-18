1389c0fe40168be500a2908f7fe1c7ebd3c636ee
Rafael Barros <rafael.rph7@gmail.com>
2017-09-18 16:17:08 -0300

    Gulp copy and clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,22 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
 
-gulp.task('build-img', function() {
+gulp.task('build-img', ['copy'], function() {
     gulp.src('src/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('src/img'));
 });
+
+gulp.task('copy', ['clean'], function() {
+    return gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean());
+})
 
 gulp.task('default', ['build-img'], function() {
     console.log("Gulp is running");
