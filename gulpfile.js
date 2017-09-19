8bea79c189e3f2e9062e3f1a54cf10793f39ed00
Rafael Barros <rafael.rph7@gmail.com>
2017-09-19 08:54:46 -0300

    Build-js and html

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,8 @@
-var gulp = require('gulp');
-var imagemin = require('gulp-imagemin');
-var clean = require('gulp-clean');
-
-gulp.task('build-img', ['copy'], function() {
-    gulp.src('src/img/**/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('src/img'));
-});
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    htmlReplace = require('gulp-html-replace');
 
 gulp.task('copy', ['clean'], function() {
     return gulp.src('src/**/*')
@@ -16,8 +12,24 @@ gulp.task('copy', ['clean'], function() {
 gulp.task('clean', function() {
     return gulp.src('dist')
         .pipe(clean());
-})
+});
 
-gulp.task('default', ['build-img'], function() {
-    console.log("Gulp is running");
-})
+gulp.task('build-img', function() {
+    gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('src/img'));
+});
+
+gulp.task('build-js', function() {
+    gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produto.js']).pipe(concat('all.js')).pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build-html', function() {
+    gulp.src('dist/**/*.html').pipe(htmlReplace({
+        js: 'all.js'
+    })).pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['copy'], function() {
+    gulp.start('build-img', 'build-js', 'build-html');
+});
